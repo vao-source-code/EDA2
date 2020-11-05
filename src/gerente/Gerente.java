ff6f8898fab35de2030177993deb5163ae5d754d
Tomasviteritti <tviteritti@alumno.unlam.edu.ar>
2020-11-05 15:53:25 -0300

    agrege junit y test, clase gerente

---
+++ src/gerente/Gerente.java
@@ -0,0 +1,46 @@
+package gerente;
+
+import calendar.MiCalendar;
+import persona.Persona;
+
+public class Gerente extends Persona{
+
+	private Integer id;
+	private Double sueldo;
+	private String sector;
+	
+	
+	public Gerente(Integer id, Double sueldo, String sector) {
+		super();
+		this.id = id;
+		this.sueldo = sueldo;
+		this.sector = sector;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public Double getSueldo() {
+		return sueldo;
+	}
+
+	public void setSueldo(Double sueldo) {
+		this.sueldo = sueldo;
+	}
+
+	public String getSector() {
+		return sector;
+	}
+
+	public void setSector(String sector) {
+		this.sector = sector;
+	}
+		
+	
+}
